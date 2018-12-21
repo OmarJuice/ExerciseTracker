d46b73646c1f609e1cd9d0e1da84f39aa1d96598
Omar Juice <ojameer1@gmail.com>
2018-12-21 10:06:30 -0500

    Updates

--- precache-manifest.65fed8ae4e84d257aa3c65c4019b7ad4.js
+++ precache-manifest.65fed8ae4e84d257aa3c65c4019b7ad4.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/ExerciseTracker/static/js/runtime~main.03214316.js"
   },
   {
-    "revision": "ccf01b74654be84327f3",
-    "url": "/ExerciseTracker/static/js/main.ccf01b74.chunk.js"
+    "revision": "d3c52bf9223fe8220075",
+    "url": "/ExerciseTracker/static/js/main.d3c52bf9.chunk.js"
   },
   {
     "revision": "5a1bd0d0b16c59d53eba",
     "url": "/ExerciseTracker/static/js/1.5a1bd0d0.chunk.js"
   },
   {
-    "revision": "ca7bcf8d1295b702db195611430c68e8",
+    "revision": "a29084aace5cefcd7394c521566534b4",
     "url": "/ExerciseTracker/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ExerciseTracker/precache-manifest.d3c7801cb84e399888985407ba270dbe.js"
+  "/ExerciseTracker/precache-manifest.65fed8ae4e84d257aa3c65c4019b7ad4.js"
 );
 
 workbox.clientsClaim();
