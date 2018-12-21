2eeb90eb0129666fe49db25fe50f27fb345b2658
Omar Juice <ojameer1@gmail.com>
2018-12-21 09:45:07 -0500

    Updates

---
+++ precache-manifest.80341fde9a525c7751bedf76e850381b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "03214316ac59c2866aa2",
+    "url": "/ExerciseTracker/static/js/runtime~main.03214316.js"
+  },
+  {
+    "revision": "5d87bc1573c49923eb2b",
+    "url": "/ExerciseTracker/static/js/main.5d87bc15.chunk.js"
+  },
+  {
+    "revision": "5a1bd0d0b16c59d53eba",
+    "url": "/ExerciseTracker/static/js/1.5a1bd0d0.chunk.js"
+  },
+  {
+    "revision": "6470fa1f54b9585c4a318ef7dd7b4fde",
+    "url": "/ExerciseTracker/index.html"
+  }
+];
